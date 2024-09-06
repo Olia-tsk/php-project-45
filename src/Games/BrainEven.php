f48ef27537a60413e7f658f34497cdd243e8ff30
Ольга Курбатова <kon@2i.tusur.ru>
2024-09-06 15:12:37 +0700

    refactor function

--- src/Games/BrainEven.php
+++ src/Games/BrainEven.php
@@ -2,18 +2,21 @@
 
 namespace BrainGames\Games\BrainEven;
 
+use function BrainGames\Cli\greetUser;
+use function BrainGames\Engine\finishGame;
+use function BrainGames\Engine\getUserAnswer;
+use function BrainGames\Engine\isCorrectAnswer;
+use function BrainGames\Engine\printGameCondition;
 use function cli\line;
-use function cli\prompt;
 
 function brainEven()
 {
-    line("Welcome to the Brain Games!");
-    $name = prompt("May I have your name?");
-    line("Hello, %s!", $name);
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $userName = greetUser();
 
-    $i = 0;
-    while ($i < 3) {
+    printGameCondition("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+
+    $counter = 0;
+    while ($counter < 3) {
         $randNumber = rand(0, 100);
         if ($randNumber % 2 == 0) {
             $correctAnswer = 'yes';
@@ -22,19 +25,15 @@ function brainEven()
         }
 
         line('Question: %s', $randNumber);
-        $answer = prompt("Your answer");
 
-        if ($answer == $correctAnswer) {
-            line("Correct!");
-            $i++;
+        $userAnswer = getUserAnswer();
+
+        if (isCorrectAnswer($correctAnswer, $userAnswer, $userName)) {
+            $counter++;
         } else {
-            line('"%s" is wrong answer ;( Correct answer was "%s".', $answer, $correctAnswer);
-            line("Let's try again, %s!", $name);
             break;
         }
 
-        if ($i == 3) {
-            line("Congratulations, %s!", $name);
-        }
+        finishGame($userName, $counter);
     }
 }
